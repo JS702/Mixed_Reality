67aee4c0a8ac75372e061eade6e985e37c87c756
JS702 <83334396+JS702@users.noreply.github.com>
2022-11-12 18:58:49 +0100

    half working ball racket interactions

--- PingPong_AR/Assets/Scripts/BallSpawner.cs
+++ PingPong_AR/Assets/Scripts/BallSpawner.cs
@@ -10,22 +10,23 @@ public class BallSpawner : MonoBehaviour
 
     public Vector3 shootDirection;
 
+    public bool isEnabled;
+
     public float shootSpeed;
     void Start()
     {
-        shootDirection = new Vector3(0,1,0);
-        shootSpeed = 0.5f;
+
     }
 
     void Update()
     {
-        if (OVRInput.GetDown(OVRInput.Button.One))//TODO gucken welcher Knopf und ob der immer auslöst oder unter bestimmten Bedingungen
+        if (isEnabled && OVRInput.GetDown(OVRInput.Button.One))//TODO gucken welcher Knopf und ob der immer auslöst oder unter bestimmten Bedingungen
         {
             if (ball)
             {
                 Destroy(ball);
             }
-            ball = Instantiate(ballPrefab);
+            ball = Instantiate(ballPrefab, transform);
             ball.GetComponent<Rigidbody>().AddRelativeForce(shootDirection * shootSpeed);
         }
     }
