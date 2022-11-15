ba720d2310d3416d9755d0c2b978b279ee3ce23d
Stefan Le <stefanle01@yahoo.de>
2022-11-15 23:25:46 +0100

    create table 2.0

--- PingPong_AR/Assets/Scripts/InputScript.cs
+++ PingPong_AR/Assets/Scripts/InputScript.cs
@@ -12,17 +12,44 @@ public class InputScript : MonoBehaviour
 
     public GameObject[] tablePoints = new GameObject[2];
 
+    public void MakeTable(Vector3 firstPoint, Vector3 secondPoint)
+    {
+        public GameObject Table = new GameObject("Table");
+        MeshFilter mf= Table.AddComponent(typeof(MeshFilter)) as MeshFilter;
+        MeshRenderer mr= Table.AddComponent(typeof(MeshRenderer)) as MeshRenderer;
+        Mesh m = new Mesh();
+        m.vertices= new Vector3[]
+        {
+        new Vector3(firstPoint.x,firstPoint.y,firstPoint.z)
+        new Vector3(firstPoint.x,firstPoint.y,secondPoint.z)
+        new Vector3(secondPoint.x,firstPoint.y,secondPoint.z)
+        new Vector3(secondPoint.x,firstPoint.y,firstPoint.z)
+        }
+        m.uv = new Vector2[]
+        {
+        new Vector2(0,0)
+        new Vector2(1,0)
+        new Vector2(0,1)
+        new Vector2(1,1)
+        }
+        m.triangles = new int[]{0,1,2,0,2,3};
+        mf.mesh =m;
+        m.RecalculateBounds();
+        m.RecalculateNormals();
+    }
+
     // Update is called once per frame
     void Update()
     {
-        // Wurde die Taste ___ auf dem Touch Controller gedrückt?
+        // Wurde die Taste ___ auf dem Touch Controller gedrï¿½ckt?
         if (OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger) && tablePoints.Length < 2)
         {
            GameObject AnkerTable =  Instantiate(objectToSpawn, OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch),Quaternion.identity);//Controller1.transform
            tablePoints[tablePoints.Length] = AnkerTable;
            if(tablePoints.Length == 2)
            {
-                MakeTable();
+                //Notiz an Nicolas: erstmal gucken, ob der cude Ã¼berhaupt spawned
+                //MakeTable(TablePoints[0],TablePoints[1]);
            }
         }
         
@@ -30,10 +57,10 @@ public class InputScript : MonoBehaviour
     public void MakeTable()
     {
         GameObject createdTable = Instantiate(table, tablePoints[0].transform.position - tablePoints[1].transform.position, Quaternion.identity);
-        // nicht vollständig
-        //Bin mir nicht sicher ob die Rotation passt,  die Mitte der Tisches ist ja einfach nur auf der Mitte des echten Tisches, aber die Ecken müssen nicht übereinstimmen
+        // nicht vollstï¿½ndig
+        //Bin mir nicht sicher ob die Rotation passt,  die Mitte der Tisches ist ja einfach nur auf der Mitte des echten Tisches, aber die Ecken mï¿½ssen nicht ï¿½bereinstimmen
     }
 
-    //TODO iwo hier einen Bereich angeben in dem die Targets spawnen können
+    //TODO iwo hier einen Bereich angeben in dem die Targets spawnen kï¿½nnen
 }
 
