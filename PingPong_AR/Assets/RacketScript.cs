2f329e110b4f33822bfeb594af8809edd73c7514
Stefan Le <stefanle01@yahoo.de>
2023-01-06 00:45:12 +0100

    switch to left hand

--- PingPong_AR/Assets/RacketScript.cs
+++ PingPong_AR/Assets/RacketScript.cs
@@ -4,26 +4,30 @@ using UnityEngine;
 
 public class RacketScript : MonoBehaviour
 {
+    Transform currentHandtoTrack;
     [SerializeField] Transform handtoTrack;
+    [SerializeField] Transform handtoTrackAlternative;
+    public bool rightHandstate=false;
     Rigidbody racketRigi;
     public bool hitBall;
     [SerializeField] AudioClip ballhitSound1;
     // Start is called before the first frame update
 
+
     void Start()
     {
         racketRigi = GetComponent<Rigidbody>();
         
-       
+        currentHandtoTrack=rightHandstate?handtoTrack:handtoTrackAlternative;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        racketRigi.velocity = (handtoTrack.position - transform.position) / Time.fixedDeltaTime;
+        racketRigi.velocity = (currentHandtoTrack.position - transform.position) / Time.fixedDeltaTime;
 
         /*
-        Quaternion roationdiff = handtoTrack.rotation * Quaternion.Inverse(transform.rotation);
+        Quaternion roationdiff = currentHandtoTrack.rotation * Quaternion.Inverse(transform.rotation);
         roationdiff.ToAngleAxis(out float angleInDeg, out Vector3 rotationAxis);
 
         Vector3 rotationDifferneceInDegree = angleInDeg * rotationAxis;
@@ -31,13 +35,13 @@ public class RacketScript : MonoBehaviour
         racketRigi.angularVelocity = (rotationDifferneceInDegree * Mathf.Deg2Rad / Time.fixedDeltaTime);
         */
         
-      //  transform.rotation = handtoTrack.rotation; // Nur zum Test -1
+      //  transform.rotation = currentHandtoTrack.rotation; // Nur zum Test -1
         
-        racketRigi.MoveRotation( handtoTrack.rotation); // Nur zum Test -1
+        racketRigi.MoveRotation( currentHandtoTrack.rotation); // Nur zum Test -1
     }
 
     //vibrationTime = Zeit in Sekunden, die der Controller vibrieren soll
-    //controller = entweder OVRInput.Controller.RHand oder OVRInput.Controller.LHand für rechts oder links
+    //controller = entweder OVRInput.Controller.RHand oder OVRInput.Controller.LHand fï¿½r rechts oder links
     public IEnumerator vibrate(float vibrationTime, OVRInput.Controller controller)
     {
         OVRInput.SetControllerVibration(0.1f, 1f, controller);
